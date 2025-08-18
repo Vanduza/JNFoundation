46e6608aac9c471d7e60679167b144a8e368362a
杨敬 <vanduza@thecover.co>
2025-08-18 16:07:45 +0800

    新增SPM支持

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "JNFoundation",
+    platforms: [
+            .iOS(.v12)
+        ],
+    products: [
+        .library(
+            name: "JNFoundation",
+            targets: ["JNFoundation"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Pircate/CleanJSON.git", from: "1.0.9"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0"),
+        .package(url: "https://github.com/Tencent/wcdb.git", from: "2.1.10"),
+        ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "JNFoundation")
+    ]
+)
